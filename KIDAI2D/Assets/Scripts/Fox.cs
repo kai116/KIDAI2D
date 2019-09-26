f456a159b0054a56fb6e554a0f5995e0c999f178
kai116 <55518563+kai116@users.noreply.github.com>
2019-09-26 12:07:24 +0800

    認識欄位語法基本類型

--- KIDAI2D/Assets/Scripts/Fox.cs
+++ KIDAI2D/Assets/Scripts/Fox.cs
@@ -3,6 +3,11 @@
 public class Fox : MonoBehaviour          //類別 類別名稱
 {
     //成員：欄位、屬性、方法、事件
+    // 修飾詞 類型 名稱 指定 值；
+    public int speed = 50;                  // 整數
+    public float jump = 2.5f;               // 浮點數
+    public string foxName = "狐狸";         // 字串
+    public bool pass = false;               // 布林值 - true/false
 
     //事件：在特定時間點會已指定頻率執行的方法
     //開始事件：遊戲開始時執行一次
