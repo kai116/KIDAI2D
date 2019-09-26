dd2e443497327599a4819bb508769b1bacb79393
kai116 <55518563+kai116@users.noreply.github.com>
2019-09-26 11:12:02 +0800

    認識c#在幹嘛

---
+++ KIDAI2D/Assets/Scripts/Fox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;                         //引用 Unity API - API 倉庫 功能、工具
+
+public class Fox : MonoBehaviour          //類別 類別名稱
+{
+    //成員：欄位、屬性、方法、事件
+
+    //事件：在特定時間點會已指定頻率執行的方法
+    //開始事件：遊戲開始時執行一次
+    private void Start()
+    {
+        
+    }
+}
