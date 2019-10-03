afa2eb4fad8963ce9d46d618cda696bce85dd3c5
kai116 <55518563+kai116@users.noreply.github.com>
2019-10-03 11:42:06 +0800

    NPC設定

---
+++ KIDAI2D/Assets/Scripts/NPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+       [Header ("對話")]
+       public string SayStart = "";
+       public string SayNotComplete = "";
+       public string SayComplete = "";
+    [Header("對話速度")]
+    public float TalkSpeed;
+    [Header("任務相關")]
+    public bool Complete;
+    public float CompletePlayer, CompleteFinish;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
